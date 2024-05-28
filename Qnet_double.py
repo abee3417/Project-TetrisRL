8e268facc9ea93e676f3e592a8ea11442e8fbfec
Yunseong Yang <abee3417@naver.com>
2024-05-28 15:26:49 +0900

    Change model logic

--- Qnet_double.py
+++ Qnet_double.py
@@ -3,9 +3,11 @@ import torch.nn as nn
 class DoubleQNetwork(nn.Module):
     def __init__(self):
         super(DoubleQNetwork, self).__init__()
-        self.conv1 = nn.Sequential(nn.Linear(4, 128), nn.ReLU(inplace=True))
-        self.conv2 = nn.Sequential(nn.Linear(128, 128), nn.ReLU(inplace=True))
-        self.conv3 = nn.Sequential(nn.Linear(128, 1))
+
+        self.conv1 = nn.Sequential(nn.Linear(4, 64), nn.ReLU(inplace=True))
+        self.conv2 = nn.Sequential(nn.Linear(64, 64), nn.ReLU(inplace=True))
+        self.conv3 = nn.Sequential(nn.Linear(64, 64), nn.ReLU(inplace=True))
+        self.conv4 = nn.Sequential(nn.Linear(64, 1))
 
         self._create_weights()
 
@@ -22,4 +24,6 @@ class DoubleQNetwork(nn.Module):
         x = self.conv1(x)
         x = self.conv2(x)
         x = self.conv3(x)
+        x = self.conv4(x)
+
         return x
--- Qnet_simple.py
+++ Qnet_simple.py
@@ -3,9 +3,11 @@ import torch.nn as nn
 class SimpleQNetwork(nn.Module):
     def __init__(self):
         super(SimpleQNetwork, self).__init__()
-        self.fc1 = nn.Sequential(nn.Linear(4, 32), nn.ReLU(inplace=True))
-        self.fc2 = nn.Sequential(nn.Linear(32, 1))
-
+        self.fc = nn.Sequential(
+            nn.Linear(4, 64),
+            nn.ReLU(inplace=True),
+            nn.Linear(64, 1)
+        )
         self._create_weights()
 
     def get_name(self):
@@ -18,6 +20,4 @@ class SimpleQNetwork(nn.Module):
                 nn.init.constant_(m.bias, 0)
 
     def forward(self, x):
-        x = self.fc1(x)
-        x = self.fc2(x)
-        return x
+        return self.fc(x)
